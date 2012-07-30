d69601d3e1e78eb38ab23ceec4c2bf65f5d50876
Thom Neale <twneale@gmail.com>
2012-07-29 22:33:40 -0400

    switch to new theme

--- flask.fortpoint.me/app.py
+++ flask.fortpoint.me/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, request, render_template
 from flask import redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_path='')
 app.debug = True
 
 
