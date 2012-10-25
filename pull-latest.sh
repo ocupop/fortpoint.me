29f1a8efd174d0e56cb5ecd37969733b4d66595c
Mike Pennisi <mike@mikepennisi.com>
2012-10-25 10:27:22 -0400

    Correct error in pull script
    Do not attempt to delete the static directoy itself (only its contents)

--- pull-latest.sh
+++ pull-latest.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 repo_dir=/home/joelgkinney/fortpoint.me
-src=/home/joelgkinney/fortpoint.me/flask.fortpoint.me/static/*
-dest=/home/joelgkinney/webapps/static/
+static_src=/home/joelgkinney/fortpoint.me/flask.fortpoint.me/static/
+static_dest=/home/joelgkinney/webapps/static/
 
 cd $repo_dir && git pull
 
 # Update static assets
 # Delete existing media.
-rm -rf $dest
+rm -rf $static_dest*
 # Copy over media from project.
-cp -R $src $dest
+cp -R $static_src* $static_dest
 
 # Restart Apache
 /home/joelgkinney/webapps/fpl_flask/apache2/bin/restart
