6a9ab679c618f55bb2b26acc9fdb276e4581ed4e
Tom Beck <tom@tastylounge.com>
2015-05-20 10:29:07 -0700

    update client list

--- clients.php
+++ clients.php
@@ -1,13 +1,13 @@
 <?php require_once('_header.php'); ?>
 
 <div class="page-header">
-  <h1 class="grid-8">Our clients are innovative companies and entrepreneurs focused on new technologies.</h1>
-  <aside class="grid-4">
+  <h1 class="primary-col">Our clients are innovative companies and entrepreneurs focused on new technologies.</h1>
+  <aside class="secondary-col">
     <p>As a trusted advisor to founders, we ensure that legal decisions promote the business and strategy goals of our clients.</p>
   </aside>
 </div>
 
-<section>
+<section class="page-content">
   <ul class="companies">
     <li>
       <a href="http://bocoup.com/" class="company">
@@ -18,6 +18,174 @@
     </li>
 
     <li>
+      <a href="http://www.bucaboot.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">secure, flexible storage for the practical biker, stop worrying about your stuff and go!</p>
+        <p class="company-name">Buca Boot</p>
+      </a>
+    </li>
+    <li>
+      <a href="http://www.thecoffeetrike.com/The_Coffee_Trike/Home.html" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">a completely self-contained and mobile, fine coffee tricycle</p>
+        <p class="company-name">The Coffee Trike</p>
+      </a>
+    </li>
+    <li>
+      <a href="http://clothbound.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">discover the best jobs in the restaurant industry</p>
+        <p class="company-name">Clothbound</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://everybodyfights.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">the boxing fitness authority</p>
+        <p class="company-name">The Club by George Foreman III</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.cuppow.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">a simple eco-friendly alternative to poor-performing and messy disposable hot cups, and over-built and expensive travel mugs</p>
+        <p class="company-name">Cuppow!</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://downtheroadbrewery.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">combining time-tested traditional European recipes with both historical and innovative brewing processes to produce locally brewed beers you’ll want to enjoy again and again</p>
+        <p class="company-name">Down the Road Brewery</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://fireflybicycles.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">world-class bicycles, handmade in boston, mass</p>
+        <p class="company-name">Firefly Bicycles</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.geekhousebikes.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">custom fit TIG welded steel frames and forks</p>
+        <p class="company-name">Geekhouse Bikes</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://goodnip.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">not your mom’s catnip</p>
+        <p class="company-name">Goodnip</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="https://www.grapevinelogic.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">collaborate with the best creators on Youtube</p>
+        <p class="company-name">Grapevine Logic</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.headquartersboston.com/" class="company">
+        <img src="../img/clients/headquarters8.png" class="wide">
+        <p class="description">a 25,000 sq ft cowering space for artisans and creative</p>
+        <p class="company-name">Headquarters Boston</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://jquery.com/" class="company">
+        <img src="../img/clients/jquery.png" class="wide"/>
+        <p class="description">moving the open web forward </p>
+        <p class="company-name">The jQuery Foundation</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.localmissioneatery.com/" class="company">
+        <img src="../img/clients/local_mission.png"/>
+        <p class="description">San Francisco’s first, locally-sourced, handmade-only grocery</p>
+        <p class="company-name">Local Mission Eatery</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.localtable.co/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">a simple turnkey web and mobile application to help you manage, schedule and optimize your workforce</p>
+        <p class="company-name">Local Table</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.nortonpoint.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">high quality eyewear inspired by an era, designed for generations, and headquartered on martha’s vineyard</p>
+        <p class="company-name">Norton Point</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.opencfmlfoundation.org/" class="company">
+        <img src="../img/clients/opencfml.png">
+        <p class="description">increasing the adoption of open source CFML solutions via advocacy and education</p>
+        <p class="company-name">Open CFML</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="https://www.onsourceonline.com/" class="company">
+        <img src="../img/clients/onsource.png" alt="OnSource">
+        <p class="description">digital photos or HD video of anything, anywhere across the nation within 24 hours</p>
+        <p class="company-name">OnSource Online</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.prismhr.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">comprehensive, enterprise software solutions to help human resource outsourcing organizations stay at the cutting edge of the industry</p>
+        <p class="company-name">Prism HR</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://simplecitystudio.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">architecture and planning in union square, Somerville</p>
+        <p class="company-name">Simple City Studio</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.parkeasier.com" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">access thousands of privately-owned parking spots that are cheaper than garages and closer to where you want to be</p>
+        <p class="company-name">SPOT</p>
+      </a>
+    </li>
+
+    <li>
+      <a href="http://www.tablr.com/" class="company">
+        <img src="http://placehold.it/250x250" class="tall"/>
+        <p class="description">a powerful table management system, allowing you to put all of your focus and energy into delighting your customers</p>
+        <p class="company-name">Tablr</p>
+      </a>
+    </li>
+  </ul>
+  <hr>
+  <ul class="companies">
+    <li>
       <a href="http://www.luvnlocal.com/" class="company">
         <img src="../img/clients/luvn_local.png" class="tall"/>
         <p class="description">shop local, stay local</p>
@@ -25,13 +193,6 @@
       </a>
     </li>
 
-    <li>
-      <a href="http://www.jquery.org" class="company">
-        <img src="../img/clients/jquery.png" class="wide"/>
-        <p class="description">supporting the advancement of the web through JavaScript</p>
-        <p class="company-name">jQuery Foundation</p>
-      </a>
-    </li>
 
     <li>
       <a href="http://www.candy.com/" class="company">
@@ -50,42 +211,10 @@
     </li>
 
     <li>
-      <a href="http://www.headquartersboston.com/" class="company">
-        <img src="../img/clients/headquarters8.png" class="wide">
-        <p class="description"> a 25,000 sq ft coworking space for artisans &amp; creatives</p>
-        <p class="company-name">Headquarters Boston</p>
-      </a>
-    </li>
-
-    <li>
-      <a href="http://www.opencfmlfoundation.org/" class="company">
-        <img src="../img/clients/opencfml.png">
-        <p class="description">increasing the adoption of open source CFML solutions via advocacy and education</p>
-        <p class="company-name">OpenCFML Foundation</p>
-      </a>
-    </li>
-
-    <li>
       <a href="https://www.good-benefits.com/" class="company">
         <img src="../img/clients/good_benefits.png" class="wide">
         <p class="description">the 401k giving plan for charity</p>
         <p class="company-name">Good Benefits</p>
-      </a>
-    </li>
-
-    <li>
-      <a href="http://www.localmissioneatery.com/" class="company">
-        <img src="../img/clients/local_mission.png"/>
-        <p class="description">San Francisco's first, locally-sourced, handmade-only grocery</p>
-        <p class="company-name">Local Mission Eatery</p>
-      </a>
-    </li>
-
-    <li>
-      <a href="http://www.onsourceonline.com/" class="company">
-        <img src="../img/clients/onsource.png" alt="OnSource">
-        <p class="description">digital photos or HD video of anything, anywhere across the nation within 24 hours</p>
-        <p class="company-name">OnSource</p>
       </a>
     </li>
 
