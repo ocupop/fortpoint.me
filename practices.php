d006664171247c6af25c914e48f45170778e2527
Tom Beck <tom@tastylounge.com>
2015-05-20 10:30:03 -0700

    add grid to practices page

--- practices.php
+++ practices.php
@@ -1,26 +1,36 @@
 <?php require_once('_header.php'); ?>
 <div class="page-header">
-  <h1 class="grid-12">We offer business & corporate legal support, as well as commercial real estate expertise</h1>
+  <h1>We offer business & corporate legal support, as well as commercial real estate expertise</h1>
 </div>
-<section>
+<section class="page-content">
   <div class="row">
-    <div class="grid-12">
-      <h4>Business & Corporate</h4>
+    <h4>Business & Corporate</h4>
+    <div class="primary-col">
+      <p>Fort Point Legal represents start-up and emerging growth companies, as well as established businesses.</p>
+      <p>Our firm advises its clients on a range of business agreements and transactions; technology licenses and contracts; and other related legal support. In financings, we advise clients on venture capital funding and private placements. Additionally, we provide support and guidance spanning mergers and acquisitions, strategic alliances, joint ventures, partnerships and other transactions as relevant to our clients' needs.</p>
+      <p>For more insight into the scope and nature of our business, please find a sampling of our clients, <a href="/clients" title="Fort Point Clients">here</a>.</p>
     </div>
-    <div class="grid-8">
-      <p>Fort Point Legal represetns start-up and emerging growth companies, as well as established businesses.</p>
-      <p>Our firm...</p>
-    </div>
-    <div class="grid-4">
-      <p>For more information about Fort Point Legal, please email: <em><a href="mailto:info@fortpointlegal.com">info@fortpointlegal.com</a></em></p>
+    <div class="secondary-col">
+      <p>For more information about Fort Point Legal, please email: <a href="mailto:info@fortpointlegal.com">info@fortpointlegal.com</a></p>
     </div>
   </div>
   <hr>
   <div class="row">
-    <div class="grid-8">
+    <div class="primary-col">
       <h4>Commercial Real Estate</h4>
-      <p>Fort Point Legal represetns start-up and emerging growth companies, as well as established businesses.</p>
-      <p>Our firm...</p>
+      <p>The practice of real estate law involves a varied and often highly complex set of activities: sales and acquisitions, title, land use, environmental factors, development, leasing, finance, joint ventures, restructuring, workouts and (at times) litigation.</p>
+      <p>The real estate practice of Fort Point Legal PC is concentrated on two
+      areas:</p>
+      <ul class="dash">
+        <li>Representation of developers and investors in the acquisition,
+      development, financing, sale and leasing of properties</li>
+        <li>Representation of real estate technology companies</li>
+      </ul>
+      <p>Experience with projects that involve ownership, syndication, taxation,
+      zoning and the regulation of land use is all used to add value to our
+      clients' transactions. We often play a prominent role in structuring (and
+      negotiating) complex real estate transactions, as well as the legal analysis
+      underlying business models real estate transactions.</p>
     </div>
   </div>
 </section>
