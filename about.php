3684f4dac3650033a1613193df9d70a16dd8cf79
Tom Beck <tom@tastylounge.com>
2015-05-20 10:28:57 -0700

    update about page

--- about.php
+++ about.php
@@ -1,17 +1,19 @@
 <?php require_once('_header.php'); ?>
 <div class="page-header">
-  <h1 class="grid-8">Entrepreneurs are best served by those who understand their vision</h1>
-  <aside class="grid-4">
+  <h1 class="primary-col">Entrepreneurs are best served by those who understand their vision</h1>
+  <aside class="secondary-col">
     <p>We focus on delivering legal services in the new economy, taking into account the way businesses are now formed, grown and exited.</p>
   </aside>
 </div>
-<section>
+<section class="page-content">
   <div class="row">
-    <div class="grid-8">
-      <p>Our clients regard Fort Point Legal...</p>
+    <div class="primary-col">
+      <p>Fort Point Legal PC was founded as a business law firm that represents startups, emerging growth companies and larger enterprises across a wide range of industries. Today, we have two practices – one spanning clients' needs in business and corporate law, and the other in commercial real estate.</p>
+      <p>We enjoy long-term relationships with both individuals and business clients, and we maintain strategic and practical relationships with a diverse array of service partners. Specifically, clients value our ability to align their legal needs with their business goals, thus making the most efficient use of time and resources.</p>
+      <p>Fort Point Legal maintains a strong commitment to the open source community, sponsoring and providing legal services to organizations that promote the success of those within this crucial technology sector.</p>
     </div>
-    <div class="grid-4">
-      <p>For more information about Fort Point Legal, please email: <em><a href="mailto:info@fortpointlegal.com">info@fortpointlegal.com</a></em></p>
+    <div class="secondary-col">
+      <p>For more information about Fort Point Legal, please email: <a href="mailto:info@fortpointlegal.com">info@fortpointlegal.com</a></p>
     </div>
   </div>
 </section>
