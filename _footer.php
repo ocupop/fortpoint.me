f436eaee03f8f903ac3d0798821d236ab1e87f71
Tom Beck <tom@tastylounge.com>
2015-05-19 18:22:27 -0700

    added footer and global styles

--- _footer.php
+++ _footer.php
@@ -1,10 +1,35 @@
 </div>
 
 <footer class="site-footer">
+  <h2>Come meet with us and develop a legal plan for your business</h2>
+  <hr>
+  <div class="row">
+    <div class='physical-address'>
+      <h4>Physical Address</h4>
+      <address>
+        51 Melcher Street<br>
+        First Floor<br>
+        Boston, MA 02210
+      </address>
+      <h4>Telephone</h4>
+      <p><a href="tel:617.765.4222">617-765-4222</a></p>
+    </div>
+    <div class='postal-address'>
+      <h4>Postal Address</h4>
+      <address>
+        PO Box 51476<br>
+        Boston, MA 02205<br>
+        &nbsp;
+      </address>
+      <h4>Facsimile</h4>
+      <p><a href="tel:484.993.2271">484-993-2271</a></p>
+    </div>
+    <div class='map'>
+      <img src="http://placehold.it/500x230/000000/ffffff">
+    </div>
+  </div>
+  <hr>
   <ul class="social-links">
-    <li>
-      <a href="index.html"><img src="img/logo.png"></a>
-    </li>
     <li>
       <a href="http://bostonbuilt.org/" target="_blank"><img src="img/BuiltinBoston.png"></a>
     </li>
@@ -14,6 +39,8 @@
     <li>
       <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank"><img src="img/by.small.png"></a>
     </li>
+    <li>Copyright 2015, Fort Point Legal PC</li>
+    <li class='right'><a href="/policy">Policy & Disclaimer</a></li>
   </ul>
 </footer>
 
--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php require_once('_header.php'); ?>
 
 <div class="page-header">
-  <h1>We have a strong commitment to the open source and entrepreneur communities.</h1>
-  <aside>
-    <p>Fort Point Legal represents the open source, internet startup and entrepreneur communities.  We work closely with our clients to offer critical business advice and cost effective legal solutions.</p>
-  </aside>
+  <h1>We are a business law firm that represents clients ranging from emerging growth companies to large, established enterprises across a wide range of industries.</h1>
 </div>
 
 <section>
@@ -68,17 +65,6 @@
     <ul id="slider-nav"></ul>
   </figure>
 
-</section>
-
-<section class="legal">
-  <div class="block">
-    <h2>Communication Policy</h2>
-    <p>While we are happy to have you contact us by electronic mail, telephone, surface mail or facsimile transmission, merely communicating with Fort Point Legal and its professionals will not create a lawyer-client relationship. Such a relationship will not arise unless and until the firm agrees with you in writing to handle a particular matter. Importantly, unless and until this has occurred, you should not convey to us any information you regard as personal or confidential, because any such information will not be treated by us as confidential or subject to any confidentiality requirements.</p>
-  </div>
-  <div class="block">
-    <h2>Legal Disclaimer</h2>
-    <p>This website provides information about our firm and certain topics that we feel users of the website may find of interest. This information may be considered advertising under the rules of professional conduct applicable to our legal practice. Any information we receive from you prior to the formal establishment of such a relationship will not be confidential. The content provided on the website is for informational purposes only. Before you make any decision that may have legal implications, you should consult with a qualified legal professional for specific legal advice tailored to your situation. Your use of this site is not an invitation to enter into and does not create an attorney-client relationship. Please do not convey to us any information you regard as confidential unless and until a formal lawyer-client relationship has been established. Any information we receive from you prior to the formal establishment of such a relationship will not be confidential.</p>
-  </div>
 </section>
 
 <?php require_once('_footer.php'); ?>
