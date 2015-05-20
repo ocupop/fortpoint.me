cb6944e42b7bab3362e49b19522135a8eea1fab0
Tom Beck <tom@tastylounge.com>
2015-05-20 10:29:39 -0700

    add all news and events

--- news.php
+++ news.php
@@ -1,6 +1,56 @@
 <?php require_once('_header.php'); ?>
 <div class="page-header">
-  <h1>We are a business law firm that represents clients ranging from emerging growth compaies to large, established enterprises across a wide range of industries.</h1>
-  <aside>some other stuff</aside>
+  <h1>News & Events</h1>
 </div>
+<section class="page-content">
+  <div class="post">
+    <h3>New Fort Point Legal Website Announcement</h3>
+    <p class="date">May 11, 2015</p>
+    <div class="summary">
+      <p>FortPointLegal.com has been redesigned to provide updated and additional information about our professional services. The following are updated or new about the Fort Point Legal website:</p>
+      <ul class="dash">
+        <li>Homepage more accurately reflects who we are and what we do</li>
+        <li>Business and Commercial RealEstate Practices highlights the depth of legal services now offered by FPL</li>
+        <li>Clients throws the spotlight on new and established clients</li>
+        <li>News and Events — features relevant local and national news and events</li>
+        <li>Connect helps you contact us</li>
+      </ul>
+      <p>Check it out and also connect with us on Twitter @FortPointLegal to receive more updates</p>
+    </div>
+  </div>
+  <div class="event">
+    <h3>Boston - Commercial / Institutional Real Estate Women's Leadership Workshop</h3>
+    <div class="summary">
+      <p>Felix / Weiner Consulting Group - Commercial / Institutional Real Estate Women's Leadership Program</p>
+      <p class="time">Wednesday, May 20, 2015 from 1:00PM to 5:00PM (EDT)</p>
+      <p><a href="http://www.eventbrite.com/e/boston-commercial-institutional-real-estate-womens-leadership-workshop-tickets-16444605257?aff=es2">Click here to view event</a></p>
+    </div>
+  </div>
+  <div class="event">
+    <h3>Boston BikeTECH 2015</h3>
+    <div class="summary">
+      <p>A Showcase of Boston's Most Innovative Bicycle Technologies</p>
+      <p class="time">Thursday, May 28, 2015 from 6:00 PM to 8:30 PM (EDT)</p>
+      <p><a href="http://www.eventbrite.com/e/boston-biketech-2015-tickets-16876075797?aff=es2">Click here to view event</a></p>
+    </div>
+  </div>
+  <div class="event">
+    <h3>WeWork Labs Demo Day</h3>
+    <div class="summary">
+      <p class="time">June 3, 2015 4:30 – 6:30</p>
+      <p><a href="https://www.eventbrite.com/e/wework-labs-demo-day-registration-16157554681">Click here to view event</a></p>
+    </div>
+  </div>
+  <div class="event">
+    <h3>Realshare BOSTON – for CRE professionals</h3>
+    <div class="summary">
+      <p>
+        <strong>Date:</strong> July 22, 2015<br>
+        <strong>Time:</strong> 07:30AM - 12:30 PM<br>
+        <strong>Location:</strong> Omni Parker House
+      </p>
+      <p><a href="http://www.globest.com/events/realshare_event/boston/RealShare-Boston-2015-356121-1.html">Click here to view event</a></p>
+    </div>
+  </div>
+</section>
 <?php require_once('_footer.php'); ?>
