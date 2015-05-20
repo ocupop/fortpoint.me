e2b3b7f53478e64ee32facb9d9c3386a8ac85e43
Tom Beck <tom@tastylounge.com>
2015-05-20 10:30:20 -0700

    add content to new policy page

--- policy.php
+++ policy.php
@@ -1,10 +1,20 @@
-<section class="legal">
-  <div class="block">
-    <h2>Communication Policy</h2>
-    <p>While we are happy to have you contact us by electronic mail, telephone, surface mail or facsimile transmission, merely communicating with Fort Point Legal and its professionals will not create a lawyer-client relationship. Such a relationship will not arise unless and until the firm agrees with you in writing to handle a particular matter. Importantly, unless and until this has occurred, you should not convey to us any information you regard as personal or confidential, because any such information will not be treated by us as confidential or subject to any confidentiality requirements.</p>
+<?php require_once('_header.php'); ?>
+<div class="page-header">
+  <h1>Policy & Disclaimer</h1>
+</div>
+<section class="page-content">
+  <div class="row">
+    <div class="primary-col">
+      <h4>Communication Policy</h4>
+      <p>While we are happy to have you contact us by electronic mail, telephone, surface mail or facsimile transmission, merely communicating with Fort Point Legal and its professionals will not create a lawyer-client relationship. Such a relationship will not arise unless and until the firm agrees with you in writing to handle a particular matter. Importantly, unless and until this has occurred, you should not convey to us any information you regard as personal or confidential, because any such information will not be treated by us as confidential or subject to any confidentiality requirements.</p>
+    </div>
   </div>
-  <div class="block">
-    <h2>Legal Disclaimer</h2>
-    <p>This website provides information about our firm and certain topics that we feel users of the website may find of interest. This information may be considered advertising under the rules of professional conduct applicable to our legal practice. Any information we receive from you prior to the formal establishment of such a relationship will not be confidential. The content provided on the website is for informational purposes only. Before you make any decision that may have legal implications, you should consult with a qualified legal professional for specific legal advice tailored to your situation. Your use of this site is not an invitation to enter into and does not create an attorney-client relationship. Please do not convey to us any information you regard as confidential unless and until a formal lawyer-client relationship has been established. Any information we receive from you prior to the formal establishment of such a relationship will not be confidential.</p>
+  <hr>
+  <div class="row">
+    <div class="primary-col">
+      <h4>Legal Disclaimer</h4>
+      <p>This website provides information about our firm and certain topics that we feel users of the website may find of interest. This information may be considered advertising under the rules of professional conduct applicable to our legal practice. Any information we receive from you prior to the formal establishment of such a relationship will not be confidential. The content provided on the website is for informational purposes only. Before you make any decision that may have legal implications, you should consult with a qualified legal professional for specific legal advice tailored to your situation. Your use of this site is not an invitation to enter into and does not create an attorney-client relationship. Please do not convey to us any information you regard as confidential unless and until a formal lawyer-client relationship has been established. Any information we receive from you prior to the formal establishment of such a relationship will not be confidential.</p>
+    </div>
   </div>
 </section>
+<?php require_once('_footer.php'); ?>
